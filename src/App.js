dd7a68971a44d603df5af86dcb8827767555e20f
Jovit-Mathew236 <jovitmathew236@gmail.com>
2022-07-04 03:31:11 +0530

    Make onboarding page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+
 import Login from './components/Login';
+import Onboarding from './components/Onboarding';
 
 function App() {
   return (
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/demo' element={<div><Login /></div>} />
+        <Route path='demo/onboarding' element={<div><Onboarding /></div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Onboarding.js
+++ src/components/Onboarding.js
@@ -1,13 +1,46 @@
 import React from 'react'
+import Search from '../assets/Search'
+import "./onboarding.css"
 
 function Onboarding() {
-  return (
-    <div>
-        <div className="page">
-            
+    return (
+        <div>
+            <div className="page">
+                <div className="p">
+                    <h1>Hire the best freelance <br />
+                        artists, online.</h1>
+                    <br />
+                    <p>Hire and meet over 7,586+ freeance artists in all Kerala</p>
+                </div>
+                <div className="search">
+                    <input type="text" placeholder='Search For Artists...' />
+                    <select name="Section" id="sectionlist" required>
+                        <option value="">Expertise</option>
+                        <option value="Expertise1">Expertise1</option>
+                        <option value="Expertise2">Expertise2</option>
+                        <option value="Expertise3">Expertise3</option>
+                        <option value="Expertise4">Expertise4</option>
+                    </select>
+                    <select name="Section" id="sectionlist" required>
+                        <option value="">Location</option>
+                        <option value="Location1">Location1</option>
+                        <option value="Location2">Location2</option>
+                        <option value="Location3">Location3</option>
+                        <option value="Location4">Location4</option>
+                    </select>
+                    <button><Search /> </button>
+                </div>
+            </div>
+            <div className="pics">
+                <div className="firstpic"> </div>
+                <div className="secpic"></div>
+                <div className="thirdpic"></div>
+
+
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Onboarding
