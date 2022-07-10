04baaea34c262a75a700e178ccf483b32bbbf321
Jovit-Mathew236 <jovitmathew236@gmail.com>
2022-07-11 00:29:07 +0530

    Updates

---
+++ precache-manifest.d81294fc356e5204bb13bf0f03ba3b73.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "64becdac37d6976136d4",
+    "url": "demo/static/css/main.dbcfef90.chunk.css"
+  },
+  {
+    "revision": "64becdac37d6976136d4",
+    "url": "demo/static/js/main.64becdac.chunk.js"
+  },
+  {
+    "revision": "e7810d1631fc9a8fd8e9",
+    "url": "demo/static/js/1.e7810d16.chunk.js"
+  },
+  {
+    "revision": "4c493a14e9dfb8b6631e",
+    "url": "demo/static/js/runtime~main.4c493a14.js"
+  },
+  {
+    "revision": "0d90c2a0f0ae9dd7f1459e63eac29ead",
+    "url": "demo/static/media/loginpic.0d90c2a0.png"
+  },
+  {
+    "revision": "af2c8a926df3f5b4f182d5a53f5f65f1",
+    "url": "demo/static/media/image 6.af2c8a92.png"
+  },
+  {
+    "revision": "4bc177976f12ee10de2863c51685bfbb",
+    "url": "demo/static/media/image 4.4bc17797.png"
+  },
+  {
+    "revision": "9a05295b7f0d21b9af4a345bb1c33ffd",
+    "url": "demo/static/media/close-up-young-successful-man-smiling-camera-standing-casual-outfit-against-blue-background 1.9a05295b.png"
+  },
+  {
+    "revision": "12d86f3c124c3a6c6c33c5ec1c5ea3d9",
+    "url": "demo/index.html"
+  }
+];
