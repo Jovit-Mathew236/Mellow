b76e4ebab103a8e61288b01c8e4169f37b64f83e
Jovit-Mathew236 <jovitmathew236@gmail.com>
2022-07-23 21:22:16 +0530

    Change The Firebase server

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,14 +8,14 @@ import  'firebase/compat/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD_KWBU1VtnAnaYocAM-P_xgTPpXjzwAWg",
-    authDomain: "mellow-68e02.firebaseapp.com",
-    projectId: "mellow-68e02",
-    storageBucket: "mellow-68e02.appspot.com",
-    messagingSenderId: "1078515858383",
-    appId: "1:1078515858383:web:033444d7e87d7b4ec4f0ba",
-    measurementId: "G-PBN3F2EMQ8"
-  };
+  apiKey: "AIzaSyBm5eWDmF25ofpUFjSDyCRePyIi0grxHrw",
+  authDomain: "mellow-d017c.firebaseapp.com",
+  projectId: "mellow-d017c",
+  storageBucket: "mellow-d017c.appspot.com",
+  messagingSenderId: "248273529453",
+  appId: "1:248273529453:web:9d32defb78feac5e22ad64",
+  measurementId: "G-NQPN2600DK"
+};
 
 export default firebase.initializeApp(firebaseConfig)
 export {firebase as Firebase}
